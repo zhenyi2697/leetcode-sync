e6ade3a688501926c76a967aea80c868bee1f497
zhenyi <zhenyi2697@gmail.com>
2025-01-30 21:35:19 -0800

    Update file 1/30/2025, 9:35:18 PM

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,14 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int threshold = nums.length / 2;
+        Integer candidate = null;
+        int count = 0;
         for(int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-            if (map.get(n) > threshold) {
-                return n;
+            if (count == 0) {
+                candidate = n;
             }
+            count += (candidate == n) ? 1 : -1;
         }
 
-        return 0;
+        return candidate;
     }
 }
